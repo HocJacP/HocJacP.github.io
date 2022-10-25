e583390d60a09a4ca20de2142b424abfd8b3efce
Hochstatter <hocjacp@dunwoody.edu>
2022-10-25 08:28:57 -0500

    Fixed formatting of JS code

--- JS/formscript.js
+++ JS/formscript.js
@@ -6,16 +6,16 @@ document.getElementById('leftImage').addEventListener('click', function () {
     document.getElementById('mainSub').innerHTML = 'RUN WHILE YOU CAN';
 });
 
-document.getElementById('submitButton').addEventListener('click', function() {
+document.getElementById('submitButton').addEventListener('click', function () {
     document.title = "YOUR SOUL IS DAMNED";
 });
 
-document.getElementById('nameInput').addEventListener('click', function() {
+document.getElementById('nameInput').addEventListener('click', function () {
     nameInput.value = null;
     nameInput.style.color = 'black';
 });
 
-document.getElementById('desc').addEventListener('click', function() {
+document.getElementById('desc').addEventListener('click', function () {
     desc.innerHTML = null;
     desc.style.color = 'black';
 });
