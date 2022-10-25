e2f25d0a2379e678254de57821cfb8e3b39ff4a0
Hochstatter <hocjacp@dunwoody.edu>
2022-10-25 07:47:34 -0500

    Added files

---
+++ JS/formscript.js
@@ -0,0 +1,7 @@
+document.getElementById('rightImage').addEventListener('click', function () {
+    document.getElementById('mainTitle').innerHTML = "IT'S TOO LATE FOR YOU"
+});
+
+document.getElementById('leftImage').addEventListener('click', function () {
+    document.getElementById('mainSub').innerHTML = 'RUN WHILE YOU CAN'
+})
