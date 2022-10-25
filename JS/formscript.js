29681ec53bdc46bcbed907b12767fed7403da7fa
Hochstatter <hocjacp@dunwoody.edu>
2022-10-25 07:55:27 -0500

    Added some JS

--- JS/formscript.js
+++ JS/formscript.js
@@ -1,7 +1,11 @@
 document.getElementById('rightImage').addEventListener('click', function () {
-    document.getElementById('mainTitle').innerHTML = "IT'S TOO LATE FOR YOU"
+    document.getElementById('mainTitle').innerHTML = "IT'S TOO LATE FOR YOU";
 });
 
 document.getElementById('leftImage').addEventListener('click', function () {
-    document.getElementById('mainSub').innerHTML = 'RUN WHILE YOU CAN'
-})
+    document.getElementById('mainSub').innerHTML = 'RUN WHILE YOU CAN';
+});
+
+document.getElementById('submitButton').addEventListener('click', function() {
+    document.title = "YOUR SOUL IS DAMNED";
+});
